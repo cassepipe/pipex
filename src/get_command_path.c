e8e69124bbc6d5e604e995941b3741e6ad5bbf56
cassepipe <cassepipe@ymail.com>
2021-09-28 00:37:35 +0200

    get_command path is now "protected"

--- src/get_command_path.c
+++ src/get_command_path.c
@@ -2,17 +2,20 @@
 
 char	*get_command_path(char *cmd, char *cwd, char **pathvar_entries)
 {
-	if (cmd[0] == '/')
+	if (cmd && *cmd)
 	{
-		return (cmd);
-	}
-	else if (ft_strchr(cmd, '/'))
-	{
-		cmd = retrieve_file_path_in_cwd(cwd, cmd);
-	}
-	else
-	{
-		cmd = retrieve_command_in_pathvar(cmd, pathvar_entries);
+		if (cmd[0] == '/')
+		{
+			return (cmd);
+		}
+		else if (ft_strchr(cmd, '/'))
+		{
+			cmd = retrieve_file_path_in_cwd(cwd, cmd);
+		}
+		else
+		{
+			cmd = retrieve_command_in_pathvar(cmd, pathvar_entries);
+		}
 	}
 	return (cmd);
 }
